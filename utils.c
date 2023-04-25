92179dc29fbedc025a46884b429bd009a4c5db89
Hana447 <alemayehuhana22@gmail.com>
2023-04-25 10:45:05 -0700

    Test

---
+++ utils.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * is_printable - function 0
+ * @c: param1
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+	return (0);
+}
+/**
+ * append_hexa_code - function 1
+ * @buffer: param1
+ * @i: param2.
+ * @ascii_code:param3
+ * Return:  3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+	return(3);
+}
+/**
+ * is_digit - function 2
+ * @c: param1
+ * Return: 1 || 0
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+/**
+ * convert_size_number - function 3
+ * @num: param1.
+ * @size: param2.
+ * Return: num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+	return ((int)num);
+}
+/**
+ * convert_size_unsgnd - function 4
+ * @num: param1
+ * @size: param2
+ * Return: number
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+	return ((unsigned short)num);
+}
